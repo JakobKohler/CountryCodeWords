8c38b896fff0bb683616de1567860eee54005be7
Jakob Kohler <schasch.dev@protonmail.com>
2024-03-16 00:56:16 +0100

    Port uses env

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import {CountryCodeFinder} from "./utils";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const checker = new CountryCodeFinder();
 
